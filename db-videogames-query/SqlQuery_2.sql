42f1ddac931608cea774090587363352525c3226
Dario Costantini <costantini95dario@gmail.com>
2023-03-29 17:56:45 +0200

    GROUP BY DONE

---
+++ db-videogames-query/SqlQuery_2.sql
@@ -0,0 +1,28 @@
+﻿SELECT COUNT(*)
+FROM software_houses
+GROUP BY software_houses.country
+
+SELECT videogames.id, COUNT(*) as reviews
+FROM videogames
+JOIN reviews ON videogames.id = reviews.videogame_id
+GROUP BY videogames.id;
+
+SELECT pegi_labels.id,
+COUNT(*) AS n_videogames
+FROM pegi_label_videogame
+GROUP BY pegi_label_id;
+
+
+SELECT release_date, COUNT(*) AS videogames
+FROM videogames
+GROUP BY year; 
+
+SELECT device_id,
+COUNT(*) as n_videogames
+FROM device_videogame
+GROUP BY device_id;
+
+SELECT videogames_id, AVG(rating) as rating
+FROM reviews
+GROUP BY videogame_id
+ORDER BY rating DESC; 
